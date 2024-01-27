c3f0341c960bcb3380d301629ab5d0b4aeb2235e
Ken Wu <kenwu1009us@gmail.com>
2024-01-26 23:01:14 -0800

    udpate setup

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cleaner",
-    version="0.1.0",
-    author="Ken Wu",
-    author_email="kenwu1009us@gmail.com",
-    description="",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/ken1009us/cleaner",
+    name="pycleancodecli",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-        # Dependencies listed in requirements.txt
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.11.5",
+    install_requires=[],
     entry_points={
         "console_scripts": [
             "cleaner=cleaner.main:main",
         ],
     },
+    author="Ken Wu",
+    author_email="kenwu1009us@gmail.com",
+    description="A tool to clean comment-out code in Python.",
+    long_description="A tool to clean Python code files from comments and unnecessary whitespace.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ken1009us/pycleancodecli",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
